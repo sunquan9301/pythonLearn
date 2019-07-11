5e5738c23740fbefa2841158a5074408cabb4a03
sunquan <sunquan9301@163.com>
2019-07-11 21:09:53 +0800

    [Feature]建立索引，并构建数据库

--- searchengine/Main.py
+++ searchengine/Main.py
@@ -5,7 +5,8 @@ from urllib import request, error, parse, robotparser, response
 def main():
     pagelist = ['http://www.hao123.com/']
     crawler = Crawler('')
-    crawler.crawl(pagelist)
+    # crawler.crawl(pagelist)
+    crawler.createIndexTables()
 
     # c = request.urlopen('http://www.hao123.com/')
     # contents = c.read()
--- searchengine/SearchEngine.py
+++ searchengine/SearchEngine.py
@@ -1,3 +1,4 @@
+import re
 from urllib import request, parse
 from bs4 import *
 import sqlite3
@@ -10,6 +11,7 @@ class Crawler:
             ssl._create_default_https_context = ssl._create_unverified_context
 
         self.con = sqlite3.connect(dbname)
+        self.ignorewords = set(['the', 'of', 'to', 'and', 'a', 'in', 'is', 'it'])
 
     def __del__(self):
         self.con.close()
@@ -25,28 +27,66 @@ class Crawler:
         self.con.execute('create table link_words(wordid,linkid)')
         self.con.execute('create index wordidx on word_list(word)')
         self.con.execute('create index urlidx on url_list(url)')
-        self.con.execute('create index wordurlidx on wordlocation(wordid)')
+        self.con.execute('create index wordurlidx on word_location(wordid)')
         self.con.execute('create index urltoidx on link(toid)')
         self.con.execute('create index urlfromidx on link(fromid)')
 
     # 获取条目的id,如果条目不存在，就将其加入数据库中
     def getentryid(self, table, field, value, createnew=True):
-        return None
+        cur = self.con.execute("select rowid from %s where %s='%s'" % (table, field, value))
+        res = cur.fetchone()
+        if res == None:
+            cur = self.con.execute("insert into %s (%s) values ('%s')" % (table, field, value))
+            return cur.lastrowid
+        else:
+            return res[0]
 
     # 为每个网页建立索引
     def addtoindex(self, url, soup):
+        if self.isindexed(url): return
         print('Indexing %s' % url)
+
+        text = self.gettextonly(soup)
+        words = self.separatewords(text)
+
+        urlid = self.getentryid('urllist', 'url', url)
+
+        for i in range(len(words)):
+            word = words[i]
+            if word in self.ignorewords: continue
+            wordid = self.getentryid('wordlist', 'word', word)
+            self.con.execute(
+                "insert into word_location(urlid,wordid,location) values (%d,%d,%d)" % (urlid, wordid, i))
 
     # 从一个HTML网页中提取文字
     def gettextonly(self, soup):
-        return None
+        v = soup.string
+        if v == None:
+            c = soup.content
+            resulttext = ""
+            for t in c:
+                subtext = self.gettextonly(t)
+                resulttext += subtext + '\n'
+            return resulttext
+        else:
+            return v.strip()
 
     # 根据任何非空白字符进行分词处理
+    """
+    只是简单的将任何非字母或非数字的字符作为分隔符
+    
+    """
+
     def separatewords(self, text):
-        return None
+        splitter = re.compile('\\W*')
+        return [s.lower() for s in splitter.split(text) if s != '']
 
     # 如果url已经建立索引，则返回true
     def isindexed(self, url):
+        u = self.con.execute("select rowid from urllist where url = '%s'" % url).fetchone()
+        if u != None:
+            v = self.con.execute('select * from word_location where urlid=%d' % u[0]).fetchone()
+            if v != None: return True
         return False
 
     # 添加一个关联两个网页的链接
@@ -82,7 +122,3 @@ class Crawler:
             pages = newpages
 
         print("end")
-
-    # 创建数据库表
-    def createindextables(self):
-        pass
